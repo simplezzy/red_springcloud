f12d150c78839b90db63935b8e9bbec99d3148dc
zhiyu.zhou <zhiyu.zhou@chinaredstar.com>
2018-01-31 14:49:04 +0800

    springcloud-hystix-dashboard

---
+++ springcloud-hystix-ribbon-dashboard/src/main/java/com/redcat/demo/config/HystrixConfig.java
@@ -0,0 +1,26 @@
+package com.redcat.demo.config;
+
+import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by zhiyu.zhou on 2018/1/31.
+ */
+@Configuration
+public class HystrixConfig {
+
+    @Bean
+    public HystrixCommandAspect hystrixAspect() {
+        return new HystrixCommandAspect();
+    }
+
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registration.addUrlMappings("/hystrix.stream");
+        return registration;
+    }
+}
---
+++ springcloud-hystix-ribbon-dashboard/src/main/java/com/redcat/demo/service/RibbonHyBoardService.java
@@ -0,0 +1,19 @@
+package com.redcat.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by zhiyu.zhou on 2018/1/31.
+ */
+@Service
+public class RibbonHyBoardService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String indexService(String name) {
+        return restTemplate.getForObject("http://service-provider/index?name=" + name, String.class);
+    }
+}
